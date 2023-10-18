aa9694c7c540ee7e918235d8c3f2b97483e272d4
vaske454 <vasilijetomovic98@gmail.com>
2023-10-18 13:42:42 +0200

    Display json as a result on the frontend.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return app(PokemonController::class)->getPokemonList(request());
 });
 
 require __DIR__.'/auth.php';
