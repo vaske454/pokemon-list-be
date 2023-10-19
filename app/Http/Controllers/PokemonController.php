2f298dfa6004076f7b145cbf1b7cb93f70ccbf28
vaske454 <vasilijetomovic98@gmail.com>
2023-10-19 12:58:16 +0200

    If table pokemons exist.

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+//use Illuminate\Support\Facades\Http;
 use App\Models\Pokemon;
+use Illuminate\Support\Facades\Schema;
 
 class PokemonController extends Controller
 {
@@ -16,23 +17,25 @@ class PokemonController extends Controller
     public function getPokemonList(Request $request): JsonResponse
     {
 
-        $paged = $_GET['paged'] ?? 1;
-
-        $perPage = 20;
-
-        $currentPage = $request->input('page', $paged);
-
-        $offset = ($currentPage - 1) * $perPage;
-
-        $pokemons = Pokemon::skip($offset)->take($perPage)->get();
-
-        $total = Pokemon::count();
-
-        $response = [
-            'pokemons' => $pokemons,
-            'current_page' => $currentPage,
-            'total' => $total,
-        ];
+        if (Schema::hasTable('pokemons')) {
+            $paged = $_GET['paged'] ?? 1;
+            $perPage = 20;
+            $currentPage = $request->input('page', $paged);
+            $offset = ($currentPage - 1) * $perPage;
+            $pokemons = Pokemon::skip($offset)->take($perPage)->get();
+            $total = Pokemon::count();
+            $response = [
+                'pokemons' => $pokemons,
+                'current_page' => $currentPage,
+                'total' => $total,
+            ];
+        } else {
+            $response = [
+                'pokemons' => [],
+                'current_page' => 1,
+                'total' => 0,
+            ];
+        }
 
         return response()->json($response);
     }
